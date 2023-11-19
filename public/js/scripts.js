f59f94b9341ff554af72e778e8968e000a6cdb49
Leonidas <leonidas.serra16@gmail.com>
2023-11-19 17:42:49 -0300

    aprimoramento da calcular para o teste npm

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,15 +11,31 @@ function back(){
     expressao = expressao.slice(0, -1);
     atualizarVisor();
 }
-function calcular(){
-    try {
-        expressao = eval(expressao).toString();
-        atualizarVisor();
-    } catch (error) {
-        expressao = "Erro";
-        atualizarVisor();
+function calcular() {
+    // Obtém o elemento #resultado
+    const resultadoElemento = document.getElementById('resultado');
+  
+    // Obtém a expressão do conteúdo do elemento
+    const expressao = resultadoElemento.innerHTML;
+  
+    // Remove espaços em branco da expressão e verifica se está vazia
+    if (!expressao.trim()) {
+      // Se a expressão estiver vazia, exibe a mensagem 'Nada para calcular'
+      resultadoElemento.innerHTML = 'Nada para calcular';
+      return;
     }
-}
+  
+    try {
+      // Tenta realizar o cálculo
+      const resultado = Function('"use strict";return (' + expressao + ')')();
+  
+      // Atualiza o conteúdo do elemento #resultado com o resultado calculado
+      resultadoElemento.innerHTML = resultado.toString();
+    } catch (error) {
+      // Em caso de erro, define o conteúdo do elemento como 'Erro'
+      resultadoElemento.innerHTML = 'Erro';
+    }
+  }
 
 // usar a seguinte função nas outras funcionalidades
 function atualizarVisor() {
